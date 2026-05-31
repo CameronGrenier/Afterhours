7c6bc8aeac7b6418be61b7f8eda6ffe67f8e335f
Dayt0w <hallman.alec13@gmail.com>
2026-05-31 19:35:09 -0400

    Adding in web socket communication, Sockets are put into rooms on join and removed on leave, discconect logic has been added to handle sudden leaving

--- Games/Game 2 Client/Client.js
+++ Games/Game 2 Client/Client.js
@@ -2,6 +2,8 @@ let userName = "" //Global variable for the username
 let roomCode = "" //Global variable for the entered room code
 const SERVER_BASE = 'http://127.0.0.1:8000' //Global URL for the server. LocalHost
 const readline = require('readline').promises;
+const {io} = require("socket.io-client");
+const socket = io(SERVER_BASE); //Start a web socket on this client
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -34,26 +36,42 @@ async function postData(endpoint = '', data = {}){
     });
     return response.json()
 }
+function startLobbyListeners(socket){
+    //This function starts client listening for players joining and leaving
+    socket.on("player_joined", (data) => {
+        console.log(`[Lobby Alert]: ${data.username} has joined the room!`);
+    });
+    socket.on("player_left", (data) => {
+        console.log(`\n[Lobby Alert]: ${data.username} has left the room.`);
+    });
+}
 
 async function startProgram() {
-
-
     let userInput = "";
+    const socket = io
     while (true) {
         //console.clear()
         await fetchServerStatus();
         console.log(`--- Main Menu ---\nUsername: ${userName}\nRoomID: ${roomCode}`);
-        userInput = await rl.question('[1] Join a room\n[2] Create a room\n[3] Exit\nEnter your command Number: ');
+        userInput = await rl.question('[1] Join a room\n[2] Create a room\n[3] Get room satus\n[4] Exit\nEnter your command Number: ');
         console.log(`You typed: ${userInput}`);
-        if (userInput === "3"){
+        if (userInput === "4"){
             break
         }
         switch(userInput) {
             case "1":
                 await checkUsername()
-                roomCode = await rl.question('Enter the room code you want to join: ');
+                const temp_roomCode = await rl.question('Enter the room code you want to join: ');
                 console.log("Joining a room...")
-                const status = await postData('/join_room', {code: `${roomCode}`, username: `${userName}`})
+                const status = await postData('/join_room', {code: `${temp_roomCode}`, username: `${userName}`, sid: `${soso}`})
+                console.log("Reply from server: ", status)
+                if (status['status'] === "success"){
+                    roomCode = status['Room Code'] //Remember the room code in memory
+                }
+                else if (status['status'] === "nameConflict"){
+                    console.log("Name is taken in this current room, please change your user name and try again")
+                    userName = "" //Reset username to make the user pick a new one
+                }
                 break;
             case "2":
                 await checkUsername()
@@ -62,8 +80,22 @@ async function startProgram() {
                 console.log("Reply from Server: ", response)
                 roomCode = response['Room Code'] //Pull out the room code from the newly created room
                 break;
+            case "3":
+                if (roomCode !== "" && userName !== ""){
+                    const response = await postData('/room_status', {code: `${roomCode}`, username: `${userName}`})
+                    console.log("Reply from server: ", response)
+                }
+                break;
+            case "4":
+                if (roomCode !== "" && userName !== "") {
+                    const response = await postData('/leave_room', {code: `${roomCode}`, username: `${userName}`});
+
+                }
+                break;
+
             default:
                 console.log("Invalid option, try again.")
+
         }
         await new Promise(resolve => setTimeout(resolve, 1000))
     }
@@ -71,4 +103,9 @@ async function startProgram() {
     console.log("Program finished");
 }
 
-startProgram();
+socket.on("connect", () => {
+    //Important verification to ensure the user socket can talk with the server socket.
+    console.log("Connected to server with Socket ID: ", socket.id)
+    startProgram();
+
+})
--- room_manager.py
+++ room_manager.py
@@ -9,6 +9,8 @@ from GameRoom import GameRoom
 
 app = FastAPI()
 sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
+active_rooms = {} #Used for tracking what rooms currently exist
+sid_to_rooms = {} #Used for tracking what SID is in what room
 socket_app = socketio.ASGIApp(sio, app)
 # Enable CORS so your Javascript (running on a different port/domain) can talk to this
 app.add_middleware(
@@ -17,17 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-#Socket Events
-@sio.event
-async def connect(sid, environ):
-    print(f"Socket connected with {sid}")
-
-@sio.event
-async def join_game_socket(sid,data):
-    code = data["code"]
-    sio.enter_room(sid, code)
-
 #Helper Functions
 def generate_room_code():
     characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789" #almost a million possible rooms at one time with a 4 digit code with these chars.
@@ -38,29 +29,37 @@ def generate_room_code():
 
 
 #HTTP Requests:
+"""General Errors:
+userError: username given is not in a room
+codeError: room code given does not exist
+error: generic error, one of the above did not happen
+"""
+class RoomData(BaseModel):
+    """username: The Users Name....
+    code: The room code the user is talking about
+    sid: The socket id of the user"""
+    username: str
+    code: Optional[str] = None
+    sid: Optional[str] = None
 
 @app.get("/status")
 def get_status():
     return {"status": "success", "message": "Python server is running"}
 
 @app.post("/room_status")
-def get_room_status(username: str, code: str):
+def get_room_status(request: RoomData):
+    code = request.code
+    username = request.username
     if code not in active_rooms:
-        return {"status": "success", "message": "Room code does not exist, must not be in a valid room"}
+        return {"status": "codeError", "message": "Room code does not exist, must not be in a valid room"}
     else:
         room = active_rooms[code]
         status = room.check_for_player(username)
         if status == "Success":
-            return {"status": "success", "message": "Room Exists and this player is in the room"}
+            return {"status": "success", "message": f"{username} is in room {code} with {active_rooms[code].players}"} #Return all users in this room
         else:
-            return {"status": "success", "message": "Room Exists but the player is not in the room"}
+            return {"status": "userError", "message": "Room Exists but the player is not in the room"}
 
-
-
-active_rooms = {}
-class RoomData(BaseModel):
-    username: str
-    code: Optional[str] = None
 @app.post("/create_room")
 def make_room(request: RoomData):
     username = request.username
@@ -74,16 +73,22 @@ def make_room(request: RoomData):
             return {"status": "success", "Room Code": code}
 
 @app.post("/join_room")
-def join_room(request: RoomData):
+async def join_room(request: RoomData):
     code = request.code
     username = request.username
+    sid = request.sid
     if code not in active_rooms:
-        return {"status": "error", "message": "Room code does not exist"}
+        return {"status": "codeError", "message": "Room code does not exist"}
     status = active_rooms[code].add_player(username)
     if status == "Success":
+        #Take the sid of the user and add it to this rooms socket channel
+        sio.enter_room(sid, code)
+        sid_to_rooms[sid] = code #Track what SIO is in what room {just code no need for the whole object)
+        #Announce to the rooom (including the current player) that they have joined
+        await sio.emit('player_joined',{'all_players': active_rooms[code].players}, room=code)
         return {"status": "success", "Room Code": code} #Important to pass the code back, the front end should remember the code
     else:
-        return {"status": "error, player name is already in use"}
+        return {"status": "nameConflict"}
 
 @app.post("/select_game")
 def select_game(game_id: str, code: str):
@@ -91,14 +96,7 @@ def select_game(game_id: str, code: str):
         room = active_rooms[code]
         room.set_game(game_id)
 
-@app.post("/leave_room")
-def leave_room(username: str, code: str):
-    if code in active_rooms:
-        status = active_rooms[code].remove_player(username)
-        if status == "Success":
-            return {"status": "success"}
-        else:
-            return {"status": "error, for some reason unable to remove player from the room."}
+
 
 @app.post("/start_game")
 def start_game(game_name: str, code: str):
@@ -109,4 +107,44 @@ def start_game(game_name: str, code: str):
     if status == "Error":
         return {"status": "error, not enough players to start the selected game"}
     return {"status": "success"}
+
+#Web Socket Events:
+
+@app.post("/leave_room")
+async def leave_room(request: RoomData):
+    """This is a combo of a HTTP Post (request to leave room)
+    and a socket call (alert all players someone has left the room)
+    Joining a lobby needs to have the same effect."""
+    code = request.code
+    username = request.username
+    sid = request.sid
+    if code in active_rooms:
+        status = active_rooms[code].remove_player(username)
+        if status == "Success":
+            # Remove the player from the socket communication room
+            sio.leave_room(sid, code)
+            #Announce the player has left and give a new players list to all clients in this room.
+            await sio.emit('player_left',{'all_players': active_rooms[code].players})
+            return {"status": "success", "message": f"{username} has been successfully removed from room {code}"}
+        else:
+            return {"status": "error, for some reason unable to remove player from the room."}
+    else:
+        return{"status":"codeError"}
+
+@sio.event
+async def connect(sid, environ):
+    print(f"Socket connected with {sid}")
+
+@sio.event
+async def disconnect(sid):
+    if sid in sid_to_rooms:
+        room_code = sid_to_rooms[sid]
+        await sio.emit('player_left',{'all_players': active_rooms[room_code].players})
+        del sid_to_rooms[sid] #Remove it from the data structure
+
+@sio.event
+async def join_game_socket(sid,data):
+    code = data["code"]
+    print(f"Socket {sid} joined the room {code}")
+
 #run this with: uvicorn filename:app --reload
