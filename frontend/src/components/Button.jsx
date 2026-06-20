4d2313538771b227a85034852fd3f249aeefcfe4
Jake Finkelstein <jake.finkelstein@asigra.com>
2026-06-19 22:32:01 -0400

    remove the button and typing sound fx impl

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,5 +1,3 @@
-import {playSound} from "../audio/playSound.jsx";
-
 /**
  * Button component used across the app for interactive actions.
  *
@@ -41,8 +39,6 @@ let base, hover, focus;
       className={`w-full rounded-md px-16 py-4 text-2xl font-bold border-2 cursor-pointer transition-colors duration-200 ${buttonStyle}`}
       aria-label={ariaLabel}
       onClick={onClick}
-      onPointerDown={() => playSound("buttonDown")}
-      onPointerUp={() => playSound("buttonUp")}
     >
     {children}
     </button>
--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,4 +1,3 @@
-import {playSound} from "../audio/playSound.jsx";
 
 function Input( { type, placeholderText, onChange } ) {
     return (
@@ -7,11 +6,6 @@ function Input( { type, placeholderText, onChange } ) {
             placeholder={placeholderText} 
             className="mx-auto block w-full rounded-md border-2 border-black bg-white py-3 text-center text-2xl text-gray-700 font-medium outline-none focus:placeholder:text-transparent"
             onChange={(e) => onChange(e.target.value)}
-            onKeyDown = {(event) => {
-                if (event.key.length === 1 || event.key === "Backspace") {
-                    playSound("keyStroke");
-                }
-            }}
         />
     )
 }
