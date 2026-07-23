bb92c89b960ed25a88c1c39c2e6d024382f87fbe
Cameron Grenier <camerongrenier12345@gmail.com>
2026-07-23 16:51:11 -0400

    remove an indent

--- frontend/src/pages/crashOut/CrashOut.jsx
+++ frontend/src/pages/crashOut/CrashOut.jsx
@@ -12,12 +12,12 @@ export default function CrashOutPage(){
     const {gameState} = useCrashOutContext();
     return(
         <div className='flex flex-col h-dvh overflow-hidden'>
-            <SettingsPanel/>
-            <MembersPanel/>
-            <header className='w-full flex justify-center items-center z-[3] py-[24px]'>
-            <h1 className='text-3xl lg:text-4xl font-display uppercase text-white'>afterhours</h1>
-            </header>
-            <CrashOutGame/>
+          <SettingsPanel/>
+          <MembersPanel/>
+          <header className='w-full flex justify-center items-center z-[3] py-[24px]'>
+          <h1 className='text-3xl lg:text-4xl font-display uppercase text-white'>afterhours</h1>
+          </header>
+          <CrashOutGame/>
         </div>
     )
 }
